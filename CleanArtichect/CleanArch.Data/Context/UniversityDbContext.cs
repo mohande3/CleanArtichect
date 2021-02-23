ba1efb7dc86062975bc3d63c8cf5a8698e736f51
mohamd taghi khademi <mohande3khademi@hotmail.com>
2021-02-23 07:44:42 +0330

    Create UniversityDbContext and connectionstring

---
+++ CleanArtichect/CleanArch.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Data.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
